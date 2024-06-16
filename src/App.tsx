ed446119839e6e6c7e46c89c7a27911bd6adfb34
da.aldridge <da.aldridge@proton.me>
2024-06-16 11:54:29 -0400

    initialized status and shortcut bar components; cleaned some css (margins, height/width, dark mode)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import './App.css'
+import ShortcutBar from './components/ShortcutBar'
+import StatusBar from './components/StatusBar'
 
 export default function App() {
 
   return (
-    <>
-      <div>
-        Hello world.
+      <div className="main">
+        <StatusBar />
+        <div>
+          
+        </div>
+        <ShortcutBar />
       </div>
-    </>
   )
 }
