dea0fb9589ef0ffd45d9832efb65198b6f0e91d1
Daniel Aldridge <da.aldridge@proton.me>
2024-06-19 22:23:03 -0400

    shortcut bar layout progress

--- src/components/HomeScreen/MainHomeArea.tsx
+++ src/components/HomeScreen/MainHomeArea.tsx
@@ -1,8 +1,8 @@
 export default function MainHomeArea() {
     
-    return(
-          <div className="h-4/5 bg-neutral-400">
-            <p className="text-3xl text-black-700">MAIN HOME AREA</p>
-          </div>
-    )
+  return(
+        <div className="h-5/6 bg-neutral-400">
+          <p className="text-3xl text-black-700">MAIN HOME AREA</p>
+        </div>
+  )
 }
--- src/components/HomeScreen/ShortcutBar.tsx
+++ src/components/HomeScreen/ShortcutBar.tsx
@@ -1,8 +1,31 @@
 export default function ShortcutBar() {
     
     return(
-        <div className="bg-neutral-600 h-1/5">
-            <p>SHORTCUT BAR</p>
+        <div className="bg-neutral-600 h-1/6 w-full flex flex-row justify-around items-center">
+                <div className="h-24 w-20 bg-red-200 flex flex-col justify-between items-center">
+                    <div>
+                        
+                    </div>
+                    <p>MESSAGES</p>
+                </div>
+                <div className="h-24 w-20 bg-green-200 flex flex-col justify-between items-center">
+                    <div>
+                    
+                    </div>
+                    <p>PHOTOS</p>
+                </div>
+                <div className="h-24 w-20 bg-blue-200 flex flex-col justify-between items-center">
+                    <div>
+                    
+                    </div>
+                    <p>CONTACTS</p>
+                </div>
+                <div className="h-24 w-20 bg-yellow-200 flex flex-col justify-between items-center">
+                    <div>
+                        
+                    </div>
+                    <p>LOGOUT</p>
+                </div>
         </div>
     )
 }
